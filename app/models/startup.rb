907ec42ba469757f950bd5808d027ba91624847b
emilioquintana90 <emilioquintana90@gmail.com>
2020-05-18 20:24:07 -0500

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,55 @@
 class Startup
+  attr_reader :name, :founder, :domain
+  @@all = []
 
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @@all << self
+  end
+
+  def pivot(domain, name)
+    @name = name
+    @domain = domain
+  end 
+
+  def sign_contract(venture_capitalist, type, investment)
+    FundingRound.new(self, venture_capitalist, type, investment.to_f)
+  end
+
+  def funding_rounds
+    FundingRound.all.select {|fr| fr.startup == self}
+  end
+
+  def num_funding_rounds
+    funding_rounds.count
+  end
+
+  def total_funds
+    (funding_rounds.map {|fr| fr.investment}).sum
+  end
+
+  def investors
+    (funding_rounds.map {|fr| fr.venture_capitalist}).uniq
+  end
+
+  def big_investors
+    investors.select {|vc| vc.total_worth}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_founder(founder_name)
+    self.all.find {|startup| startup.founder == founder_name}
+  end
+
+  def self.domains
+    self.all.collect {|startup| startup.domain}
+  end
+
+  private
+  attr_writer :domain, :name
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,26 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+# ********* STARTUP INSTANCES ************
+twitter = Startup.new("Twitter", "Jack Dorsey", "Social Media")
+instagram = Startup.new("Instagram", "Kevin Systrom", "Social Media")
+evernote = Startup.new("Evernote", "Bill Gates", "Note Taking")
+
+# ********* VENTURE CAPITALIST INSTANCES ************
+ray = VentureCapitalist.new("Ray Dalio", 5000000000)
+warren = VentureCapitalist.new("Warren Buffett", 10000000000)
+tim = VentureCapitalist.new("Tim Ferris", 50000000)
+
+# ********* FOUNDING ROUND INSTANCES ************
+FundingRound.new(twitter, ray, "Type A", 50000)
+FundingRound.new(twitter, warren, "Type A", 25000)
+
+FundingRound.new(instagram, tim, "Type A", 30000)
+FundingRound.new(instagram, ray, "Type A", 45000)
+
+FundingRound.new(evernote, tim, "Type A", 30000)
+FundingRound.new(evernote, warren, "Type A", 45000)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
